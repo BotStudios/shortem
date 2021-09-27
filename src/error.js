6aa91287cee573d4beff2570e0ff404832cfd955
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2021-09-27 12:19:02 +0800

    Update error.js

--- src/error.js
+++ src/error.js
@@ -1,4 +1,6 @@
-const config = require('./../config.json');
+const config = require('./config.json');
+const Database = require('./utils/database.js');
+const database = new Database();
 require('dotenv').config();
 
 
@@ -6,3 +8,7 @@ if(!config.token && !process.env.token)throw new Error("Please Provide A Token")
 if(!config.prefix && !process.env.prefix)throw new Error("Please Provide A Prefix");
 if(!config.owner_id && !process.env.owner_id)throw new Error("Please Provide An Owner ID");
 if(!config.PORT && !process.env.PORT)throw new Error("Please Provide A Port To Listen");
+
+(async () => {
+  if(!Array.isArray(await database.list()))throw new Error("Please Specify Database Methods\nExample : https://github.com/BotStudios/shortem/tree/main/example")
+})()
